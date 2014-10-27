c50625dcaff642eb1353ae9a18cf6edb807304ff
Julia Zuara <julia_any@hotmail.com>
2014-10-27 08:56:44 +0100

    Primera version, crea persona, introduce edad, comprueba

---
+++ Persona.java
@@ -0,0 +1,23 @@
+
+/**
+ * Permite crear personas con diferentes edades
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Persona
+//Objeto persona con un atributo edad, se introduce al crearla.
+{
+private int edad;
+
+public Persona(int suEdad)
+//Crea una persona, introduce su edad.
+{
+edad = suEdad;
+}
+public int getEdad()
+//Devuelve la edad de la persona.
+{
+return edad;
+}
+}
